883017b38d3d5b592401b47f0eb93d19e0d46106
josusp0 <josuesalinassp0@gmail.com>
2024-03-24 18:20:36 -0600

    condiciones

---
+++ condicionesparte1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+int num1, num2;
+num1=10;
+num2= 20;
+if (num1>num2){
+	printf("El numero 1 es mayor que el numero 2");
+}
+else if (num2>num1){
+	printf("El numero 2 es mayor que el numero 1");
+}
+	return 0;
+}
+
